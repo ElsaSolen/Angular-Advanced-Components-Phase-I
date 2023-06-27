c7f7ab61080cbac912110d5e3ebf7ab5dced1ef2
ElsaSolen <97947759+ElsaSolen@users.noreply.github.com>
2023-06-27 15:18:47 +0100

    initializing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CountdownComponent } from './countdown/countdown.component';
+import { ProgressBarComponent } from './progressbar/progress-bar.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, CountdownComponent, ProgressBarComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
