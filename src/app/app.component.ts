f09d91f788083216cb8d9043a7bbcb8bf6ed47b4
ElsaSolen <97947759+ElsaSolen@users.noreply.github.com>
2023-06-28 12:22:31 +0100

    style modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   progressPercentage = 0;
-  totalCountdown = 0;
+  totalCountdown: number = null;
 
   constructor() {}
 
